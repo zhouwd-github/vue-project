35ab312d1b2fdd7b253a69d16888bf96aa008821
alexwczhou <alexwczhou@tencent.com>
2022-12-11 15:44:03 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  <div id="app">
+    <router-link to="home">home-test</router-link> ||
+    <router-link to="about">about-test</router-link> ||
+    <router-view></router-view>
+  </div>
+
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+// import HelloWorld from './components/HelloWorld.vue'
+//
+// export default {
+//   name: 'App',
+//   components: {
+//     HelloWorld
+//   }
+// }
 </script>
 
 <style>
